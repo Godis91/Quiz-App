05afeb1c1ae1cbbb326921b943ae6b954467d3be
= <=>
2019-05-11 22:38:06 +0100

    Added our routes

--- app.js
+++ app.js
@@ -7,6 +7,45 @@ const app = express();
 //initialize Json body parser for our form inputs
 app.use(express.json());
 
+//post request
+//@route is /api/questions
+//this route gets us the questions
+app.get('/api/test', (req, res) => {
+  fs.readFile('questions.json', 'utf8', (error, data) => {
+    if (error) return res.status(501).send('Server error');
+
+    res.send(data);
+  });
+});
+
+//get request
+//@route /api/answers
+// this route gets us the answers
+app.get('/api/answers', (req, res) => {
+  fs.readFile('answers.json', 'utf8', (error, data) => {
+    if (error) {
+      return res.status(501).send('Server Error');
+    }
+
+    res.send(data);
+  });
+});
+
+//Post request
+//@route is api/user
+//allows us take in user records
+app.post('/user', (req, res) => {
+  const { name, email, address } = req.body;
+
+  const user = {
+    name,
+    email,
+    address
+  };
+
+  res.send(user);
+});
+
 //create heroku and localhost ports
 const port = process.env.PORT || 4000;
 
